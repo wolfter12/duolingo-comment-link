536d9a5c0e8eecfd4e22343cce55e9f2d682dfe3
Nazar <wolfter12@gmail.com>
2020-08-18 01:26:27 +0300

    deleted console.log

--- duolingo-comment-link/background.js
+++ duolingo-comment-link/background.js
@@ -1,8 +1,7 @@
 "use strict";
 
 chrome.webNavigation.onHistoryStateUpdated.addListener(function (details) {
-  console.log("updated");
-  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     chrome.tabs.sendMessage(tabs[0].id, { action: "update" });
   });
 });
