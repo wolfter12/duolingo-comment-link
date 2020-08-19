3d84c024f0675f16e173da42335f6604d2b7d17c
Nazar <wolfter12@gmail.com>
2020-08-20 00:58:36 +0300

    popup only on forum.duolingo.com, added default extension property

--- duolingo-comment-link/background.js
+++ duolingo-comment-link/background.js
@@ -1,7 +1,30 @@
 "use strict";
 
-chrome.webNavigation.onHistoryStateUpdated.addListener(function (details) {
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.storage.local.set({
+    animation: true,
+    iconName: "classic",
+    iconSize: "20"
+  });
+
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: { hostEquals: "forum.duolingo.com" },
+      })
+      ],
+      actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+  });
+});
+
+chrome.webNavigation.onHistoryStateUpdated.addListener(() => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     chrome.tabs.sendMessage(tabs[0].id, { action: "update" });
   });
+});
+
+chrome.storage.onChanged.addListener(() => {
+  const message = "You need to refresh the page to apply the new settings.";
+  alert(message);
 });
